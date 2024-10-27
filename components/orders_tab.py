012873f694f8ba8b5a277aa285fb1c01d3353c34
willalone <159647207+willalone@users.noreply.github.com>
2024-10-27 12:57:40 +0300

    Update orders_tab.py

--- components/orders_tab.py
+++ components/orders_tab.py
@@ -1,1 +1,7 @@
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout)
 
+class OrdersTab(QWidget):
+  def __init__(self, db_manager, parent=None):
+    super().__init__(parent)
+    self.db_manager = db_manager
+    self.layout = QVBoxLayout(self)
