fab95ec0bfc28a9819ca199a659c63060e2d82eb
willalone <159647207+willalone@users.noreply.github.com>
2024-10-29 19:53:11 +0300

    Update main_window.py

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,13 +1,29 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QTabWidget, QVBoxLayout, QMenuBar, QMenu, QAction, QMainWindow, QApplication)
+from PyQt5.QtWidgets import (QWidget, QTabWidget, QVBoxLayout, QMenuBar, QMenu, QAction, QMainWindow, QApplication, QLabel)
 from PyQt5.QtGui import QFont, QIcon
 from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
-    def __init__(self, app, parent=None):
-        super().__init__(parent)
-        self.app = app
-        self.layout = QVBoxLayout(self)
+    def __init__(self, app):
+        super().__init__()
+        self.app = QApplication.instance() # Получаем текущий экземпляр QApplication
+        self.initUI()
+
+    def initUI(self):
+        # Создаем виджеты
+        label = QLabel("Это QLabel")
+        # Создаем горизонтальный макет
+        hbox = QVBoxLayout()
+        hbox.addWidget(label)
+
+        # Вкладки
+        self.tabs = QTabWidget(self)
+        hbox.addWidget(self.tabs) # Добавляем вкладки в макет
+
+        # Устанавливаем centralWidget
+        centralWidget = QWidget()
+        centralWidget.setLayout(hbox) # Устанавливаем макет на центральный виджет
+        self.setCentralWidget(centralWidget) 
 
         # Меню
         self.menubar = self.menuBar() # Получаем меню-бар из QMainWindow
@@ -23,18 +39,11 @@ class MainWindow(QMainWindow):
         # Действия меню
         exitAction = QAction(QIcon('exit.png'), 'Выход', self)
         exitAction.setShortcut('Ctrl+Q')
-        exitAction.triggered.connect(self.app.quit) # Используем app
+        exitAction.triggered.connect(self.app.quit) 
         self.fileMenu.addAction(exitAction)
-
-        # Вкладки
-        self.tabs = QTabWidget(self)
-        self.layout.addWidget(self.tabs)
-
-        self.setCentralWidget(QWidget()) #  Установка centralWidget для QVBoxLayout 
-        self.centralWidget().setLayout(self.layout) # Установка layout на centralWidget
 
 if __name__ == '__main__':
     app = QApplication(sys.argv) # Создаем экземпляр QApplication
-    main_window = MainWindow(app) # Передаем app в MainWindow
+    main_window = MainWindow() # Не нужно передавать app в MainWindow
     main_window.show()
-    sys.exit(app.exec_()) 
+    sys.exit(app.exec_())
